d2a5d55e5eea21ae15e6234b8e41a625dd974e1e
Claudia El Khatib <claudiacv22@gmail.com>
2017-10-05 19:04:47 -0400

    Successful get one trip action

--- src/Indextrips.js
+++ src/Indextrips.js
@@ -9,29 +9,6 @@ class Indextrips extends Component {
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleDelete = this.handleDelete.bind(this);
   }
-  // handleSubmit(event) {
-  //   event.preventDefault();
-  //   const apiBaseUrl = 'http://localhost:4741'
-  //   console.log(this.state)
-  //   axios({
-  //     // trip id
-  //     url: apiBaseUrl + '/trips',
-  //     method: 'GET',
-  //     headers: {
-  //       'Authorization': 'Token ' + this.props.getUser().token
-  //     },
-  //     data: {
-  //       'trip': this.state
-  //     }
-  //   })
-  //   .then(function(response) {
-  //       if (response.status >= 400) {
-  //           throw new Error("Bad response from server");
-  //       }
-  //       console.log(response.data)
-  //       return response;
-  //   })
-  // }
   handleSubmit(event) {
     event.preventDefault();
     const get = this.props.getUser()
--- src/Showtrip.js
+++ src/Showtrip.js
@@ -1,37 +1,58 @@
 import React, {Component} from 'react';
 import axios from 'axios';
 
-class Showtrip extends Component {
+const initialState = {}
 
+class Showtrip extends Component {
+  constructor(props) {
+    super(props)
+    this.state = initialState;
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+      this.setState({id: event.target.value});
+      console.log('id ', event.target.value)
+    }
   handleSubmit(event) {
     event.preventDefault();
-    const apiBaseUrl = 'http://localhost:4741'
-    console.log(this.state)
+    const get = this.props.getUser()
+    const setState = this.setState.bind(this)
+
     axios({
-      // trip id
-      url: apiBaseUrl + '/trips',
-      method: 'GET',
-      headers: {
-        'Authorization': 'Token ' + this.props.getUser().token
-      },
-      data: {
-        'trip': this.state
-      }
-    })
-    .then(function(response) {
-        if (response.status >= 400) {
-            throw new Error("Bad response from server");
+        url: 'http://localhost:4741/trips/' + this.state.id,
+        method: 'GET',
+        headers: {
+          'Authorization': 'Token ' + get.token
         }
-        console.log(response.data)
-        return response;
-    })
-  }
+      })
+      .then(function(response) {
+          if (response.status >= 400) {
+              throw new Error("Bad response from server");
+          }
+          console.log(response.data)
+          setState(response.data);
+      })
+    }
   render () {
     return (
+    <div>
       <form onSubmit={this.handleSubmit}>
         <h2>Get one trip</h2>
-        <input type="button" value="Get one trip" />
+        <input name="id" type="text" value={this.state.id || ''} onChange={this.handleChange}  />
+        <input type="submit" value="Get one trip" />
       </form>
+        {this.state.trip && <div>
+          <p>Place: {this.state.trip.place_name}</p>
+          <p>Departing date: {this.state.trip.departing_date}</p>
+          <p>Returning date: {this.state.trip.returning_date}</p>
+          <p>Travel duration: {this.state.trip.travel_duration}</p>
+          <p>Transportation: {this.state.trip.transportation}</p>
+          <p>Accomodation: {this.state.trip.accomodation}</p>
+          <p>Eat: {this.state.trip.eat}</p>
+          <p>Activities: {this.state.trip.activities}</p>
+        </div>}
+      </div>
     )
   }
 }
